cb7c0d72a4cf5973c1de3f546460a072740e0a18
heywecome <heywecome@163.com>
2021-08-12 14:48:42 +0800

    file-loader配置

---
+++ webpack/03_webpack_resource/src/js/element.js
@@ -0,0 +1,15 @@
+// import "css-loader!../css/style.css"
+import "../css/style.css"
+import "../css/title.less"
+import "../css/image.css"
+
+const divEl = document.createElement("div");
+divEl.className = "title";
+divEl.innerHTML = "你好啊，李银河";
+
+// 设置背景图片
+const bgDivEl = document.createElement('div');
+bgDivEl.className = "image-bg";
+
+document.body.appendChild(divEl);
+document.body.appendChild(bgDivEl);
---
+++ webpack/03_webpack_resource/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"), // 必须是绝对路径
+    filename: "bundle.js" // 打包之后的名字
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,// 正则表达式
+        // 1.loader的写法（语法糖）
+        // loader: "css-loader"
+        // loader其实是语法糖，完整写法：
+
+        use: [ // 写在后面的 loader 先加载
+          // {loader: "css-loader"},
+          "style-loader",
+          "css-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.less$/,// 正则表达式
+        use: [ // 写在后面的 loader 先加载
+          // {loader: "css-loader"},
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
+      },
+      {
+        // 图片资源
+        test: /\.(jpg|png|gif|svg|jpe?g)/,
+        use: "file-loader"
+      }
+    ]
+  }
+}
